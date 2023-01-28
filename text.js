dcb4625b283e079a4301b6444277b35e5fd98616
Gutierrez Julieta <julimaxizoe@gmail.com>
2023-01-28 12:24:52 -0300

    commiteando desafio1 Alura

---
+++ text.js
@@ -0,0 +1,70 @@
+const textArea = document.querySelector(".text-area");
+const diamante2 = document.querySelector(".diamante2");
+const btnCopiar = document.querySelector(".btn-copiar");
+
+
+
+
+//`La letra "e" es convertida para "enter"`
+//`La letra "i" es convertida para "imes"`
+//`La letra "a" es convertida para "ai"`
+//`La letra "o" es convertida para "ober"`
+//`La letra "u" es convertida para "ufat"`
+
+
+
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value);
+    diamante2.value = textoEncriptado;
+    textArea.value = "";
+    diamante2.style.backgroundImage = "none";
+}
+
+
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"],["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase()
+    for(let i = 0; i <matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0],[matrizCodigo[i][1]]);
+
+        }
+
+    }
+    return stringEncriptada;
+}
+
+function btnDesencriptar(){
+    const textoDesencriptado = desencriptar(textArea.value);
+    diamante2.value = textoDesencriptado;
+    textArea.value = "";
+    
+}
+
+
+function desencriptar(stringDesencriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"],["u", "ufat"]];
+    stringEncriptada = stringDesencriptada.toLowerCase()
+    for(let i = 0; i <matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+
+        }
+
+    }
+    return stringDesencriptada;
+}
+
+function btncopiar(){
+    diamante2.select();
+    navigator.clipboard.writeText(diamante2.value);
+    diamante2.value = "";
+    alert("Texto Copiado");
+}
+
+
+
+
+
+
